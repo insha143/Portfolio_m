cb553ae8277dd21cdc2b3be99655b4efcc5f9392
Insha <muhammad.inshaofficial@gmail.com>
2025-09-07 07:48:17 -0700

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,23 +1,23 @@
-const logotext = "JOHN";
+const logotext = "Kami developer";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+     title: "Kami Web Development",
+    description: "Portfolio website of Kami Web Development, showcasing web development, UI/UX, and design projects.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "Kami Developers & Designers",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "We love coding",
+        second: "We code cool websites",
+        third: "We develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: " We provide creative and effective digital solutions—ranging from web development and UI/UX design to graphic design and branding—helping businesses and individuals bring their ideas to life online",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "Hi, We are, a passionate web developer and graphic designer. I love creating beautiful, user-friendly websites and eye-catching designs that help businesses and individuals stand out. With a focus on creativity, functionality, and attention to detail, I turn ideas into digital experiences that make an impact.",
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
@@ -41,7 +41,7 @@ const skills = [{
         value: 90,
     },
     {
-        name: "Djano",
+        name: "WordPress",
         value: 85,
     },
     {
@@ -50,7 +50,7 @@ const skills = [{
     },
     {
         name: "React",
-        value: 60,
+        value: 95,
     },
     {
         name: "Jquery",
@@ -60,85 +60,78 @@ const skills = [{
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "We create intuitive and visually appealing interfaces that provide seamless user experiences, turning complex ideas into simple, engaging designs using tools like Figma, Adobe XD, Sketch, and InVision.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Web Developer",
+        description: "We build responsive, fast, and user-friendly websites and applications using React.js and Node.js, bringing ideas to life and helping businesses grow online.",
     },
     {
         title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "We create custom WordPress websites that are visually appealing, easy to manage, and fully optimized for performance using popular plugins like Elementor, WooCommerce, Yoast SEO, and Contact Form 7",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+    img: "/4.jpg",
+    description: "Created  an online book-selling platform with search, secure checkout, and responsive design.",
+    link: "https://online-shopping-xi-six.vercel.app/",
+      
+},
+    {
+        img: "/5.png",
+        description: "Developed   a medical laboratory management system covering test booking, reporting, and complete process tracking.",
+        link: "https://medical-labortray-setup.vercel.app/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "/6.png",
+        description: "Designed  an online platform for a travel agency with package listings, bookings, and enhanced digital presence",
+        link: "https://ezhar-travel-and-tourism.vercel.app/",
+    },
+   
+    {
+        img: "/7.png",
+        
+        description: "Built   a web platform for car washing and related services with booking, service listings, and responsive design.car wash",
+        link: "https://car-washing-services.vercel.app/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        
+        img: "/11.png",
+        description: "Database security protects sensitive data, ensures integrity, and prevents unauthorized access.",
         link: "#",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "/12.png",
+        description: "Design in web development is important because it enhances user experience, builds trust, and makes websites more engaging and effective.",
+        link: "#",
+    },
+    
+    {
+        img: "/8.png",
+        description: "Implemented backend security using tools like Helmet, Passport, JWT, bcrypt, and CSRF protection to ensure safe authentication and data integrity.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "/7.png",
+        description: "The 7 wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
+
+
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        
+        img: "/9.jpg",
+        description: "Secured WordPress sites using plugins and practices like Wordfence, SSL, strong authentication, and regular updates to protect against vulnerabilities.",
         link: "#",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "muhammad.inshaofficial143@gmail.com",
+    YOUR_FONE: "+966 572857593",
+    description: "Whether it’s a web project, design idea, or just a question feel free to reach out. I’m here to help make your vision a reality!",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,9 +140,9 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/insha143",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/muhammad-insha-8a86ab2a2/",
     twitter: "https://twitter.com",
 };
 export {
